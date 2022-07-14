5e27b3339847e627fc34407990e51cc1d4f5275d
Sampo Antila <sampo.antila@iki.fi>
2022-07-15 00:06:24 +0300

    dates updated and packages

--- services/bookingService.js
+++ services/bookingService.js
@@ -295,7 +295,7 @@ class BookingService {
             from: 'aslan@aslan.fi',
             templateId: config.SG_INVITE_TEMPLATE_ID,
             dynamic_template_data: {
-                date: '25.-27.3.2022'
+                date: '4.-6.8.2022'
             }
         };
     }
