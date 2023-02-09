46d05279bc15e4582646247d8a5025ae3998e2ae
Sampo Antila <sampo.antila@vincit.fi>
2023-02-09 10:37:51 +0200

    updated dates

--- services/bookingService.js
+++ services/bookingService.js
@@ -295,7 +295,7 @@ class BookingService {
             from: 'aslan@aslan.fi',
             templateId: config.SG_INVITE_TEMPLATE_ID,
             dynamic_template_data: {
-                date: '20.-22.10.2022'
+                date: '23.-25.2.2023'
             }
         };
     }
